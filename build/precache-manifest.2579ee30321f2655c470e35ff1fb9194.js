d7721461ad7eddf3f9f139c7f2ec9ad4097dcee6
adnan-321 <shaikabdullahadnan@gmail.com>
2021-08-31 21:11:13 +0530

    Added Build for deployment

---
+++ build/precache-manifest.2579ee30321f2655c470e35ff1fb9194.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d916b4d799ab2e976b2da1fd0b874532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01005ac602259112372b",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "7c11aa52e87f23598bab",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "01005ac602259112372b",
+    "url": "/static/js/2.d6ee2a89.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.d6ee2a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c11aa52e87f23598bab",
+    "url": "/static/js/main.7e062b8b.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
